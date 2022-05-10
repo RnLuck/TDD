a56e24ead725988cffee24766224773539c7b9dd
CaiDongyun <2323084497@qq.com>
2022-05-10 08:51:15 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):    # (1)
     def setUp(self):    # (3)
@@ -14,30 +16,32 @@ class NewVisitorTest(unittest.TestCase):    # (1)
         self.browser.get('http://localhost:8000')
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)   # (4)
-        header_text = self.browser.find_element_by_tag_name(' h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do ', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id(' id_new_item ')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder '),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
         # She types "Buy peacock feathers" into a text box (Edith's hobbyis tying fly-fishing lures)
-        inputbox.send_keys ( 'Buy peacock feathers ' )
+        inputbox.send_keys('Buy peacock feathers')
         # when she hits enter，the page updates，and now the page lists
          # "1:Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id(' id.list_table ')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1:Buy peacock feathers' for row in rows)
         )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
         self.fail('Finish the test!')
+
         # The page updates again，and now shows both items on her list
         #
         # Edith wonders whether the site will remember her list. Then she sees
@@ -46,6 +50,14 @@ class NewVisitorTest(unittest.TestCase):    # (1)
         #
         # She visits that URL - her to-do list is still there.
         # Satisfied,she goes back to sleep
-        
+
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')  # 1
+    #     html = response.content.decode('utf8')  # 2
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+    #     self.assertTemplateUsed(response,'home.html') #3
+
 if __name__=='__main__':
     unittest.main(warnings='ignore')
