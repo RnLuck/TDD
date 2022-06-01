187cf74ee065c50c6e8f5a6941f926d6bcd2ae79
CaiDongyun <2323084497@qq.com>
2022-06-01 19:36:21 +0800

    8-10 (3)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -109,6 +109,7 @@ class NewVisitorTest(LiveServerTestCase):    # (1)
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024,768)
 
+        
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id( 'id_new_item' )
         self.assertAlmostEqual(
@@ -116,6 +117,14 @@ class NewVisitorTest(LiveServerTestCase):    # (1)
             512,
             delta=10
             )
+
+        #She starts a new list and sees the input is nicely 
+        # centered ther too
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1:testing')
+        self.wait_for_row_in_list_table('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/ 2, 
+            512,
+            delta=10
+            )
